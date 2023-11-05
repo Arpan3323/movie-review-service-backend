711333f250dcb5394221bb30948abc2eb265cac8
Arpan Srivastava <azs0239@auburn.edu>
2023-11-05 16:53:48 -0600

    first commit

---
+++ src/main/java/com/isitwatchable/watchable/WatchableApplication.java
@@ -0,0 +1,13 @@
+package com.isitwatchable.watchable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatchableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WatchableApplication.class, args);
+	}
+
+}
