a3d80114a00d5c42434bf8eb198be631928232dc
Arpan Srivastava <azs0239@auburn.edu>
2023-11-11 17:51:03 -0600

    review functionality added

--- src/main/java/com/isitwatchable/watchable/Review.java
+++ src/main/java/com/isitwatchable/watchable/Review.java
@@ -16,6 +16,10 @@ public class Review {
     @Id
     private ObjectId id;
     private String reviewText;
+    
+    public Review(String reviewText) {
+        this.reviewText = reviewText;
+    }
 
     
 }
