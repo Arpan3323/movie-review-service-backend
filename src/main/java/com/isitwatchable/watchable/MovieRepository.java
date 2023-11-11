882c2b613e4f153cac274553e63d79903d51b7c4
Arpan Srivastava <azs0239@auburn.edu>
2023-11-11 15:53:37 -0600

    changed search by db object id to imdbId

--- src/main/java/com/isitwatchable/watchable/MovieRepository.java
+++ src/main/java/com/isitwatchable/watchable/MovieRepository.java
@@ -2,10 +2,14 @@ package com.isitwatchable.watchable;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+
 import org.bson.types.ObjectId;
 
 @Repository
 public interface MovieRepository extends MongoRepository<Movie, ObjectId>{
-
+    Optional<Movie> findByImdbId(String imdbId);
     
 }
